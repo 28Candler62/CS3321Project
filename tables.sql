a002d578effcc07dec8e8c83a7c1643abca17707
28Candler62 <jcandler2862@gmail.com>
2026-04-15 15:04:55 -0500

    tables progress update

--- tables.sql
+++ tables.sql
@@ -17,3 +17,28 @@ CREATE TABLE client (
 	CONSTRAINT			Client_PK			PRIMARY KEY(ClientID)
 	);
     
+CREATE TABLE shopper (
+	ShopperID				Integer				NOT NULL AUTO_INCREMENT,
+	ShopperFirstName		Char(35)			NOT NULL,
+	ShopperLastName			Char(35)			NOT NULL,
+	CONSTRAINT			Shopper_PK			PRIMARY KEY(ShopperID)
+	);
+    
+CREATE TABLE store (
+	StoreID				Integer				NOT NULL AUTO_INCREMENT,
+	StoreName			Char(35)			NOT NULL,
+	CONSTRAINT			Store_PK			PRIMARY KEY(StoreID)
+	);
+    
+CREATE TABLE inventory (
+	ItemID				Integer				NOT NULL AUTO_INCREMENT,
+	ItemName			Char(35)			NOT NULL,
+    ItemDescription		Char(70)			NOT NULL,
+    ItemStore			Integer				NOT NULL,
+    Quantity  		    Integer 	       	NOT NULL,
+	Price  			    Decimal(9,2)     	NOT NULL,
+    CONSTRAINT 		    ItemStore_FK 	FOREIGN KEY (ItemStore)
+							REFERENCES  store (StoreID)
+								ON UPDATE CASCADE,
+	CONSTRAINT			Item_PK			PRIMARY KEY(ItemID)
+	);
